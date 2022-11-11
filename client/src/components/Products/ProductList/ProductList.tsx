5e89af516846d00f1290df0b35fa3b0cce899d2f
Roberto <roberto.cantu1997@gmail.com>
2022-11-11 14:06:34 -0600

    Fixed conflcits

--- client/src/components/Products/ProductList/ProductList.tsx
+++ client/src/components/Products/ProductList/ProductList.tsx
@@ -90,131 +90,113 @@ function ReportModal(props: SimpleDialogProps) {
 	);
 }
 
-function ProductList() {
-	//const [products, setProducts] = useState([{ title: "test", contract: 0 }]);
+//const [products, setProducts] = useState([{ title: "test", contract: 0 }]);
 
-	function ProductList(props: Props) {
-		const navigate = useNavigate();
-		const [products, setProducts] = useState<Product[]>([
-			{
-				Contrato: 0,
-				Descripcion: "",
-			},
-		]);
+function ProductList(props: Props) {
+	const navigate = useNavigate();
+	const [products, setProducts] = useState<Product[]>([
+		{
+			Contrato: 0,
+			Descripcion: "",
+		},
+	]);
 
-		useEffect(() => {
-			const api = async () => {
-				const customHeaders: any = {
-					Authorization: "Bearer " + localStorage.getItem("accessToken"),
-					"Content-Type": "application/x-www-form-urlencoded",
-					nuc: localStorage.getItem("nuc"),
-				};
-				const data = await fetch("http://localhost:3000/api/ruta/products", {
-					method: "GET",
-					headers: customHeaders,
-				});
-				const jsonData = await data.json();
-				if (Object.prototype.toString.call(jsonData) === "[object Array]") {
-					setProducts(jsonData.data);
-				} else {
-					setProducts([jsonData.data]);
-				}
+	useEffect(() => {
+		const api = async () => {
+			const customHeaders: any = {
+				Authorization: "Bearer " + localStorage.getItem("accessToken"),
+				"Content-Type": "application/x-www-form-urlencoded",
+				nuc: localStorage.getItem("nuc"),
 			};
-			api();
-		}, []);
-
-		const [open, setOpen] = useState(false);
-		const [id, setId] = useState(-1);
-		const [contract, setContract] = useState("");
-		const [message, setMessage] = useState("");
-
-		const handleClickOpen = (id: number, contract: string) => {
-			setId(id);
-			setContract(contract);
-			setOpen(true);
-		};
-
-		const handleClickClose = (message: string) => {
-			setOpen(false);
-			setMessage(message);
-			if (message !== "close") {
-				reportProduct(id, contract, message);
+			const data = await fetch("http://localhost:3000/api/ruta/products", {
+				method: "GET",
+				headers: customHeaders,
+			});
+			const jsonData = await data.json();
+			if (Object.prototype.toString.call(jsonData) === "[object Array]") {
+				setProducts(jsonData.data);
+			} else {
+				setProducts([jsonData.data]);
 			}
 		};
+		api();
+	}, []);
 
-		// useEffect(() => {
-		//   //setProducts(product);
-		// }, [{ title: "test", contract: 0 }]);
+	const [open, setOpen] = useState(false);
+	const [id, setId] = useState(-1);
+	const [contract, setContract] = useState("");
+	const [message, setMessage] = useState("");
 
-		return (
-			<>
-				<div className="container">
-					<div className="products">
-						{products?.map((product: Product, index: number) => (
-							<Card
-								key={index}
-								sx={{ width: 1 / 5, height: 150 }}
-								className="product"
-							>
-								<CardContent>
-									<Typography
-										sx={{ fontSize: 14 }}
-										color="text.secondary"
-										gutterBottom
-									>
-										{product.Descripcion}
-									</Typography>
-									<Typography variant="body2">
-										Contract: {product.Contrato}
-									</Typography>
-								</CardContent>
-								<CardActions className="buttons">
-									<Button
-										variant="outlined"
-										size="medium"
-										color="warning"
-										sx={{ color: "red" }}
-										onClick={() =>
-											reportProduct(
-												product.Contrato,
-												product.Descripcion,
-												"hola"
-											)
-										}
-									>
-										Reportar
-									</Button>
-									{/* <Button
-                  variant="contained"
-                  size="medium"
-                  onClick={() => {
-                    navigate("1");
-                  }}
-                >
-                  Ver más
-                    // reportProduct(product.contract, product.title, "hola")
-                    // TODO: handleClickOpen(product.Contrato, product.Descripcion) .  Este es para el merge de las queries
-                    handleClickOpen(product.contract, product.title)
-                  }
-                >
-                  Report
-                </Button> */}
-									<Button variant="contained" size="medium">
-										See More
-									</Button>
-								</CardActions>
-							</Card>
-						))}
-					</div>
-					<ReportModal
-						open={open}
-						onClose={handleClickClose}
-						message={message}
-					/>
+	const handleClickOpen = (id: number, contract: string) => {
+		setId(id);
+		setContract(contract);
+		setOpen(true);
+	};
+
+	const handleClickClose = (message: string) => {
+		setOpen(false);
+		setMessage(message);
+		if (message !== "close") {
+			reportProduct(id, contract, message);
+		}
+	};
+
+	// useEffect(() => {
+	//   //setProducts(product);
+	// }, [{ title: "test", contract: 0 }]);
+
+	return (
+		<>
+			<div className="container">
+				<div className="products">
+					{products?.map((product: Product, index: number) => (
+						<Card
+							key={index}
+							sx={{ width: 1 / 5, height: 150 }}
+							className="product"
+						>
+							<CardContent>
+								<Typography
+									sx={{ fontSize: 14 }}
+									color="text.secondary"
+									gutterBottom
+								>
+									{product.Descripcion}
+								</Typography>
+								<Typography variant="body2">
+									Contract: {product.Contrato}
+								</Typography>
+							</CardContent>
+							<CardActions className="buttons">
+								<Button
+									variant="outlined"
+									size="medium"
+									color="warning"
+									sx={{ color: "red" }}
+									onClick={() =>
+										// reportProduct(product.Contrato, product.Descripcion, "hola")
+										handleClickOpen(product.Contrato, product.Descripcion)
+									}
+								>
+									Reportar
+								</Button>
+								<Button
+									variant="contained"
+									size="medium"
+									onClick={() => {
+										navigate("1");
+									}}
+								>
+									Ver mas
+								</Button>
+							</CardActions>
+						</Card>
+					))}
 				</div>
-			</>
-		);
-	}
+				<ReportModal open={open} onClose={handleClickClose} message={message} />
+			</div>
+		</>
+	);
 }
 
 export default ProductList;
